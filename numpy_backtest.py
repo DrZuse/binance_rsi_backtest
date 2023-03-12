9eedb17fc7c73a83062f9f9412c7a7427441c8dc
DrZuse <kirillshelest@gmail.com>
2023-03-12 12:55:46 +0200

    qtalib fix

--- numpy_backtest.py
+++ numpy_backtest.py
@@ -29,8 +29,8 @@ def read_csv_inparalel(file):
 
 logger.info('start read csvs')
 with Pool(cpu_count()) as p:
-    #files = p.map(read_csv_inparalel, theOrderBookFiles)
-    files = p.map(read_csv_inparalel, theOrderBookFiles[-3:]) # TEST 3 files
+    files = p.map(read_csv_inparalel, theOrderBookFiles)
+    #files = p.map(read_csv_inparalel, theOrderBookFiles[-1:]) # TEST 3 files
 logger.info('finish read csvs')
 
 
@@ -39,16 +39,21 @@ big_arr = np.concatenate(files, axis=0)
 #df['time UTC'] = pd.to_datetime(df['Open time'], unit='ms', origin='unix')
 
 logger.info(big_arr.shape)
+#logger.info(big_arr[:, 4].shape)
 
-rsi_values = ta.RSI(big_arr[:, 4]) # values 1-13 irrelevant
-#logger.info(rsi_values)
-rsi_values[0:14] = None # in respect to irrelevant values
+rsi_values = ta.RSI(big_arr[:, 4]) # BUG: return minus one value from the start. BUG: values 1-13 irrelevant 
+#logger.info(rsi_values.shape) # 
+#logger.info(rsi_values) # 
+rsi_values = np.append(rsi_values[0], rsi_values) # FIX: return minus one value from the start
+#logger.info(rsi_values.shape) # 
+#logger.info(rsi_values) # 
+rsi_values[0:14] = None # FIX: values 1-13 irrelevant 
 signals = np.where(rsi_values < 25)
 
 
 #logger.info(signals[0].tolist())
-list_of_signals = signals[0].tolist()
-logger.info(f'number of signals: {len(list_of_signals)}')
+big_arr_signal_indexes = signals[0].tolist()
+logger.info(f'len of big_arr_signal_indexes: {len(big_arr_signal_indexes)}')
 
 
 profits = 0
@@ -56,27 +61,23 @@ losses = 0
 trade_exit = 0
 not_finished_trades = 0
 
-profit_percent = 0.05
-loss_percent = 0.05
+profit_percent = 0.1
+loss_percent = 0.02
 
 logger.info('start loop')
 
-for s, sig in enumerate(list_of_signals):
-    #print(f'current  sig {sig}')
-    #print(f'current -1  {sig-1}')
-    #print(f'previous {list_of_signals[s-1]}' )
-    #print(f'previous {list_of_signals[s-1]}' )
-    #print(f'if? {list_of_signals[s-1] == sig-1}' )
-    if sig > trade_exit and list_of_signals[s-1] == sig-1:
-        logger.info(f'time_of_signal: {datetime.datetime.utcfromtimestamp(big_arr[(sig+1), 0]/1000)}')
+for s, sig_i in enumerate(big_arr_signal_indexes):
+    if sig_i > trade_exit and big_arr_signal_indexes[s-1] == sig_i-1:
+        #logger.info(f'time_of_signal: {datetime.datetime.utcfromtimestamp(big_arr[(sig_i), 0]/1000)}')
 
-        buy_price = big_arr[(sig+1), 1] # open price
-        max_high = big_arr[(sig+1), 2]
-        max_low = big_arr[(sig+1), 3]
+        # enter position at the open of next second
+        buy_price = big_arr[(sig_i+1), 1] # open price
+        max_high = big_arr[(sig_i+1), 2]
+        max_low = big_arr[(sig_i+1), 3]
         loss = (buy_price - max_low) * 100 / buy_price
         profit = (max_high - buy_price) * 100 / buy_price
         
-        for m, big_arr_m in enumerate(big_arr[(sig+1):]):
+        for m, big_arr_m in enumerate(big_arr[(sig_i+1):]):
         
             if big_arr_m[2] > max_high:
                 max_high = big_arr_m[2]
@@ -87,31 +88,25 @@ for s, sig in enumerate(list_of_signals):
                 loss = (buy_price - max_low) * 100 / buy_price
 
             if profit >= profit_percent:
+                #print('--- TP ---')
                 profits += 1
 
             if loss >= loss_percent:
+                #print('--- SL ---')
                 losses += 1
 
             if profit >= profit_percent or loss >= loss_percent:
-                print(f'buy_price: {buy_price}')
-                print(f'profit: {profit} || loss: {loss}')
-                print(f'max_high: {max_high} || max_low: {max_low}')
-                print(f'time of the end a trade: {datetime.datetime.utcfromtimestamp(big_arr_m[0]/1000)}')
+                #print(f'buy_price: {buy_price}')
+                #print(f'profit: {profit} || loss: {loss}')
+                #print(f'max_high: {max_high} || max_low: {max_low}')
+                #print(f'time of the end a trade: {datetime.datetime.utcfromtimestamp(big_arr_m[0]/1000)}')
 
-                trade_exit = (sig+1) + m
+                trade_exit = (sig_i+1) + m
 
                 break
         else:
             not_finished_trades += 1
 
-            
-            
-
-
-
-
-        #if i % 30_000 == 0:
-        #    logger.info(f'profits: {profits} || loss: {losses} || {datetime.datetime.utcfromtimestamp(big_arr_i[0]/1000)}')
 
 logger.info(f'profitable trades: {profits} - {profits*profit_percent} \n\
 lose trades: {losses} - {losses*loss_percent} \n\
